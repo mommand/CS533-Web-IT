9111f15379fa8ea6df3f18a39950591d64bd979b
Ziaullah Momand <zia@Ziaullahs-MacBook-Pro.local>
2020-11-08 13:25:24 +0430

    fist time commit

---
+++ form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form method="post" action="controller.php">
+	<label>Age</label><br>
+	<select name="age">
+		<option value="20">20</option>
+		<option value="30">30</option>
+		<option value="40">40</option>
+		<option value="50">50</option>
+	</select><br>
+	<input type="submit" name="" value="check Age">
+</form>
+</body>
+</html>
